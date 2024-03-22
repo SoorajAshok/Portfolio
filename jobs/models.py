f20aeccdaa59560e6c1ea763c9aed6c146cbd21c
Sooraj Ashok <soorajashok@iGhost.local>
2024-03-21 21:23:18 -0300

    Template Edit #2

--- jobs/models.py
+++ jobs/models.py
@@ -3,4 +3,8 @@ from django.db import models
 # Create your models here.
 class Job(models.Model):
     profile_Image = models.ImageField(upload_to = 'images/')
+    #profile
     profile_Summary = models.CharField(max_length = 800)
+
+    def __str__(self):
+        return self.profile_Summary
